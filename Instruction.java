e80d31f7c42b5836a4f19e47b505083afa331b48
ics-user <cgserrano@up.edu.ph>
2017-03-30 18:04:50 +0800

    Added Instruction syntax checker

--- Instruction.java
+++ Instruction.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Instruction{
- 
+ 	private static HashSet<String> supportedTypes = new HashSet<String>(4);
 	private String instructionType;
 	private String param1;
 	private String param2;
 
 	public Instruction(String inst){
+		fillSupportedTypes();
 		String newInst = inst.replaceAll("[,\\s\\n]+", " ");
 		String [] parser = newInst.split(" ");
 		if(parser.length != 3) { 
@@ -17,9 +19,20 @@ public class Instruction{
 			System.out.println("Register error!");
 			System.exit(0);	
 		}
+		if(!supportedTypes.contains(parser[0])){
+			System.out.println("Cannot find symbol: " + parser[0]);
+			System.exit(0);	
+		}
 		this.instructionType = parser[0];
 		this.param1 = parser[1];
 		this.param2 = parser[2];
+	}
+
+	private void fillSupportedTypes(){
+		supportedTypes.add("LOAD");
+		supportedTypes.add("SUB");
+		supportedTypes.add("ADD");
+		supportedTypes.add("CMP");
 	}
 
 	public void printInstruction(){
