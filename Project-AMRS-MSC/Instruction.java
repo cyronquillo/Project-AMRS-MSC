79da24dca1b5550f179c584f169f8a3d4c55865f
ertzworm <justinepaulguiao@gmail.com>
2017-03-30 17:01:32 +0800

    Created Instruction object

---
+++ Project-AMRS-MSC/Instruction.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class Instruction{
+
+	private String instructionType;
+	private String param1;
+	private String param2;
+
+	public Instruction(String inst){
+		String newInst = inst.replaceAll("[,\\s]+", " ");
+		String [] parser = newInst.split(" ");
+		if(parser.length != 3) { return;}
+		System.out.println(parser.length);
+		this.instructionType = parser[0];
+		this.param1 = parser[1];
+		this.param2 = parser[2];
+		System.out.println(instructionType + "  "  + param1 +  "  " +param2);
+		
+	}
+
+
+}
