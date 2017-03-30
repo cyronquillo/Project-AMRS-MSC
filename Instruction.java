33f11dbbf80641711fe7590e20a4f4f58dc44ddf
chacychavez <cschavez@up.edu.ph>
2017-03-30 17:43:05 +0800

    parsing

--- Instruction.java
+++ Instruction.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class Instruction{
  
 	private String instructionType;
@@ -8,14 +9,21 @@ public class Instruction{
 	public Instruction(String inst){
 		String newInst = inst.replaceAll("[,\\s\\n]+", " ");
 		String [] parser = newInst.split(" ");
-		if(parser.length != 3) { return;}
-		System.out.println(parser.length);
+		if(parser.length != 3) { 
+			System.out.println("Parsing error!");
+			System.exit(0);
+		}
+		if(!Main.registers.containsKey(parser[1])){
+			System.out.println("Register error!");
+			System.exit(0);	
+		}
 		this.instructionType = parser[0];
 		this.param1 = parser[1];
 		this.param2 = parser[2];
-		// System.out.println(instructionType + "  "  + param1 +  "  " +param2);
 	}
 
-
+	public void printInstruction(){
+		System.out.println(instructionType + "  "  + param1 +  "  " +param2);	
+	}
 
 }
