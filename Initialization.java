a671d7a43196a0c6b1aa670e73975bbb1f3695b5
cyronquillo <cbronquillo@up.edu.ph>
2017-04-27 13:19:37 +0800

    Fully functional out of order execution pipelining

--- Initialization.java
+++ Initialization.java
@@ -1,5 +1,6 @@
 package instantiation;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Map;
@@ -7,10 +8,12 @@ import java.util.HashMap;
 
 public class Initialization{
 	public static HashMap<String,Integer> registers = new HashMap<String,Integer>();
-	public static LinkedList<Instruction> instructions = new LinkedList<Instruction>();
+	public static ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+	public static ArrayList<ArrayList<Instruction>> clockcycle= new ArrayList<ArrayList<Instruction>>();
 	public Initialization(String file){
 		initialize(); // registers
 		readFile(file); //file reading
+		buildClockCycles();
 	}
 
 	public static void readFile(String file){
@@ -19,13 +22,12 @@ public class Initialization{
 			String line;
 			while((line= input.readLine())!= null){
 				Instruction inst = new Instruction(line.toUpperCase());
-				instructions.push(inst);
-				inst.printInstruction();
+				instructions.add(inst);
 			}
 		}
 		catch(Exception ex){}
-		instructions.get(0).printInstruction();
 	}
+
 	public static void initialize(){
 		registers.put("R1",0);
 		registers.put("R2",0);
@@ -67,6 +69,48 @@ public class Initialization{
 		registers.put("ZF",0);
 	}
 
+	public void buildClockCycles(){
+		int clock = 0;
+		int limit;
+		while(instructions.get(instructions.size()-1).getStatus() != Instruction.END){
+			clockcycle.add(new ArrayList<Instruction>());
+			limit = clock+1 < instructions.size()? clock+1: instructions.size();
+			for(int i = 0; i < limit; i++){
+				if(instructions.get(i).getStatus() < Instruction.END){
+					clockcycle.get(clock).add(instructions.get(i));
+				}
+			}
+
+			for(int i = 0; i <  clockcycle.get(clock).size(); i++){
+				clockcycle.get(clock).get(i).setStall(false);
+				for(int j = 0; j < i; j++){
+					if(clockcycle.get(clock).get(i).getStatus() == Instruction.START) break;
+					if( checkRAW(clockcycle.get(clock).get(j), clockcycle.get(clock).get(i)) || 
+							checkWAR(clockcycle.get(clock).get(j), clockcycle.get(clock).get(i)) || 
+								checkWAW(clockcycle.get(clock).get(j), clockcycle.get(clock).get(i)) || 
+								checkDuplicateStage(clockcycle.get(clock).get(j), clockcycle.get(clock).get(i))) {
+						clockcycle.get(clock).get(i).setStall(true);
+						break;
+					}
+
+				}
+				if(!clockcycle.get(clock).get(i).getStall()){
+					clockcycle.get(clock).get(i).setStatus(
+						clockcycle.get(clock).get(i).getStatus() + 1 
+					);
+
+				}				
+			}
+			if(instructions.get(instructions.size()-1).getStatus() != Instruction.END) System.out.println("Clock Cycle " + clock + ": ");
+			for(int i = 0; i < clockcycle.get(clock).size(); i++){
+				clockcycle.get(clock).get(i).printStatus();
+			}
+			// System.out.println(instructions.get(instructions.size()-1).getStatus());
+
+			clock++;
+		}			
+	}
+
 	public static void store(String reg, int value){
 		registers.replace(reg,value);
 	}
@@ -75,4 +119,34 @@ public class Initialization{
 		System.out.println(registers.get(reg));
 	}
 
+	public boolean checkWAW(Instruction inst1, Instruction inst2){
+		if(inst1.getParam1().equals(inst2.getParam1()) && inst1.getStatus() != Instruction.END) {
+			// System.out.println("WAW");
+			return true;
+		}
+		return false;
+	}
+	public boolean checkWAR(Instruction inst1, Instruction inst2){
+		if(inst1.getParam2().equals(inst2.getParam1()) && inst1.getStatus() != Instruction.END){
+			// System.out.println("WAR");
+			return true;
+		}
+		
+		return false;
+	}
+	public boolean checkRAW(Instruction inst1, Instruction inst2){
+		if(inst1.getParam1().equals(inst2.getParam2()) && inst1.getStatus() != Instruction.END){
+			// System.out.println("RAW");
+			return true;
+		}
+		return false;
+	}
+	public boolean checkDuplicateStage(Instruction inst1, Instruction inst2){
+		if(inst1.getStatus() == inst2.getStatus() + 1 && !inst1.getStall() && inst1.getStatus() != Instruction.END){
+			// System.out.println("Stage Duplicate");
+			return true;
+		}
+		return false;
+	}
+	
 }
--- Instruction.java
+++ Instruction.java
@@ -2,11 +2,21 @@ package instantiation;
 import java.util.ArrayList;
 import java.util.HashSet;
 public class Instruction{
+	public static final int START = 0;
+	public static final int FETCH = 1;
+	public static final int DECODE = 2;
+	public static final int EXECUTE = 3;
+	public static final int MEMORY = 4;
+	public static final int WRITEBACK = 5;
+	// public static final int END = 4;
+	public static final int END = 6;
+
  	private static HashSet<String> supportedTypes = new HashSet<String>(4);
 	private String instructionType;
 	private String param1;
 	private String param2;
-
+	private int status;
+	private boolean stalled;
 	public Instruction(String inst){
 		fillSupportedTypes();
 		String newInst = inst.replaceAll("[,\\s\\n]+", " ");
@@ -23,16 +33,83 @@ public class Instruction{
 			System.out.println("Cannot find symbol: " + parser[0]);
 			System.exit(0);	
 		}
+		this.status = START;
+		this.stalled = false;
 		this.instructionType = parser[0];
 		this.param1 = parser[1];
 		this.param2 = parser[2];
 	}
+
+	// public Instruction(Instruction copy){
+	// 	this.instructionType = copy.instructionType;
+	// 	this.param1 = copy.param1;
+	// 	this.param2 = copy.param2;
+	// 	this.status = copy.status;
+	// 	this.stalled = copy.status;
+	// }
 
 	private void fillSupportedTypes(){
 		supportedTypes.add("CMP");
 		supportedTypes.add("LOAD");
 		supportedTypes.add("SUB");
 		supportedTypes.add("ADD");
+	}
+
+	public int getStatus(){
+		return this.status;
+	}
+
+	public void setStatus(int status){
+		this.status = status;
+	}
+
+	public void printStatus(){
+		if(this.status != END){
+			System.out.print("\t" +instructionType + "  "  + param1 +  "  " +param2 + ": " );	
+			if(this.stalled == true){
+				System.out.println("STALL");
+				return;
+			}
+			switch(this.status){
+				case FETCH:
+					System.out.println(" FETCH");
+					break;
+				case DECODE:
+					System.out.println(" DECODE");
+					break;
+				case EXECUTE:
+					System.out.println(" EXECUTE");
+					break;
+				case MEMORY:
+					System.out.println(" MEMORY");
+					break;
+				case WRITEBACK:
+					System.out.println(" WRITEBACK");
+					break;
+				default:
+					System.out.println(" ERR");
+			}
+		}
+	}
+
+	public void setStall(boolean toStall){
+		this.stalled = toStall;
+	}
+
+	public String getParam1(){
+		return this.param1; 
+	}
+
+	public String getParam2(){
+		return this.param2; 
+	}
+
+	public String getInstructionType(){
+		return this.instructionType; 
+	}
+
+	public boolean getStall(){
+		return this.stalled;
 	}
 
 	public void printInstruction(){
