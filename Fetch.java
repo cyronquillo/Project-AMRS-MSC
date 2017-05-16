edb7c5f17c1bfe0406f38af9fc29ba7b071f6338
cyronquillo <cbronquillo@up.edu.ph>
2017-05-16 14:23:33 +0800

    documented Fetch;

--- Fetch.java
+++ Fetch.java
@@ -8,9 +8,9 @@ public class Fetch{
 
 
 	public void process(Instruction toFetch){
-		Initialization.MAR = Initialization.PC;
-		Initialization.MBR = Initialization.instructions.get(toFetch.getAddress());
-		Initialization.PC++;
+		Initialization.MAR = Initialization.PC; // changes the value of MAR to the current value of PC
+		Initialization.MBR = Initialization.instructions.get(toFetch.getAddress()); // gets  the value of MBR corresponding to MAR
+		Initialization.PC++; // increments the value of PC
 	}
 
 
