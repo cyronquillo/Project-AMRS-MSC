9d28cdf5ff91b004bb8be0dcf9bdcc5c874d3957
NorielSotto26 <nesotto@up.edu.ph>
2017-05-16 14:35:31 +0800

    added documentation to Decode.java

--- Decode.java
+++ Decode.java
@@ -2,27 +2,40 @@
 package instantiation;
 public class Decode{
 
-	private String op1Var;					
-	private int op1Val;																					//holds the value of operand 1
-	private int op2;																					//holds the value of operand 2
+	private String op1Var;		
+	//String that holds the register name of the first operand
+	private int op1Val;					
+	//Int variable that holds the value of the register of the first operand
+	private int op2;	
+	//Int variable that holds the value of the register of the second operand																				
 
 	public Decode(){}
-
+	
+	//Method used for decoding previous fetched instruction
+	//Parameter: instruction
 	public void process(Instruction toDecode){
 		this.op1Var = toDecode.getParam1();
+		//assigns First Register Name to class variable
 		this.op1Val = getRegisterValue(toDecode.getParam1());
+		//assigns First Register Value to class variable
 
-		if(isRegister(toDecode.getParam2())) this.op2 = getRegisterValue(toDecode.getParam2());			//if operand 2 is a register
-		else this.op2 = Integer.parseInt(toDecode.getParam2());											//if operand 2 is an immediate value
+		if(isRegister(toDecode.getParam2())) this.op2 = getRegisterValue(toDecode.getParam2());
+		//if second operand is a register
+		//get the value of that register and assign that value
+		else this.op2 = Integer.parseInt(toDecode.getParam2());	
+		//if second operand is an immediate value
+		//parse the string, and immediately assgin the value
 
-		Initialization.CIR = toDecode.getInstructionType();												//assigns instruction type to CIR
-		// printResult();
+		Initialization.CIR = toDecode.getInstructionType();
+		//assign the instruction type to the CIR
 	}
-
+	//returns true if operand is a register
+	//parameter: String operand
 	public boolean isRegister(String op){
 		return Initialization.registers.containsKey(op);
 	}
-
+	//returns the value of the register
+	//parameter: String operand
 	public int getRegisterValue(String op){
 		return Initialization.registers.get(op);
 	}
@@ -32,7 +45,6 @@ public class Decode{
 		System.out.println("Operand 1: " + op1Val);
 		System.out.println("Operand 2: " + op2);
 	}
-
 
 	public int getOp1Val(){
 		return this.op1Val;
