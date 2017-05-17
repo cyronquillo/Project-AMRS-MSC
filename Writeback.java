8c4f2a1e19ca2e5a9aeca45769b4fd9cf245cd28
Honey Grace Garay <hggaray@up.edu.ph>
2017-05-17 11:45:59 +0800

    Documented Writeback

--- Writeback.java
+++ Writeback.java
@@ -12,11 +12,11 @@ public class Writeback{
 	}
 
 	public void process(){
-		value = Initialization.memory.getValue();
-		setZF = Initialization.memory.getZF();
-		setOF = Initialization.memory.getOF();
-		setNF = Initialization.memory.getNF();
-		destination = Initialization.memory.getDestination();
+		setZF = Initialization.memory.getZF();								//get value of zero flag from memory
+		setOF = Initialization.memory.getOF();								//get value of overflow flag from memory
+		setNF = Initialization.memory.getNF();								//get value of negative flag from memory
+		value = Initialization.memory.getValue();							//get value of executed instruction from memory
+		destination = Initialization.memory.getDestination();				//get register destination from memory
 		
 		if(value==100){				//value==100 means that cmp is used
 			changeFlags();			//so write to flag only
